bd0c74142f2f1875c9f65ad35ff0ef3c872061d7
EliaBuratti <elia.buratti.dev@gmail.com>
2023-08-23 15:46:48 +0200

    add calculator cost without validator number

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+/*
+ Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 
@@ -8,4 +9,56 @@ va applicato uno sconto del 20% per i minorenni
 
 va applicato uno sconto del 40% per gli over 65.
 
-L'output del prezzo finale va stampato in forma umana (ovvero con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca nella documentazione di JS. */
+L'output del prezzo finale va stampato in forma umana (ovvero con massimo due decimali, per indicare i centesimi sul prezzo).
+
+Questo richiederà un minimo di ricerca nella documentazione di JS. 
+*/
+
+//prompt km + prompt età con number
+
+const userKm = Number(prompt('Quanti km vuoi fare?'));
+
+const userAge = Number(prompt('Quanti anni hai?'));
+
+//valido i dati inseriti affinchè siano numeri e nel caso da un avviso poi ricarico la pagina.
+
+/*
+***PROVA****
+if (userKm != Number() && userAge != Number()) {
+    alert('Non fare il furbo!');
+    window.location.reload()
+} */
+
+//costante costo al km
+const priceKm = 0.21;
+
+//costante di sconto
+let discount;
+
+
+// if/else per definire gli sconti 
+
+if (userAge <= 18) { //sconto minorenni
+    discount = 20;
+
+} else if (userAge > 65) { //sconto over 65
+    discount = 40;
+
+} else { //nessuno sconto
+    discount = 0;
+}
+
+//calcolo il prezzo da scontare
+const priceDIscount = ((userKm * priceKm ) * discount) / 100;
+
+//costo del biglietto con l'eventuale sconto
+const subPrice = (userKm * priceKm ) - priceDIscount;
+
+
+//stampare risultato al massimo di due decimali
+
+const finalPrice = subPrice.toFixed(2);
+
+console.log(finalPrice + '€');
+
+document.getElementById('yourCost').innerHTML = 'Il costo del tuo biglietto è: ' + finalPrice + '€';
